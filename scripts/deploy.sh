415611eb67bce7def19833f5c13916c2b320b21a
June <1601745371@qq.com>
2023-11-07 02:24:07 +0800

    deploy by June at 2023-11-07 02:24:07

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
+###
+ # @Author: June
+ # @Description: 
+ # @Date: 2023-11-06 01:48:03
+ # @LastEditors: June
+ # @LastEditTime: 2023-11-07 02:23:36
+### 
 curPath='/Users/june/Desktop/my_blog'
 outPath='/Users/june/Desktop/blog_build'
 
 # read -p '请输入操作:' input
 
-if [ $1 = 'p_myblog' ]; then
+if [ $1 = 'cur' ]; then
     cd $curPath
     echo '开始提交仓库'
     git add .
     git commit -am "deploy by June at `date '+%Y-%m-%d %H:%M:%S'`"
-    git push -f origin master
+    git push origin master
     echo '提交完成'
 elif [ $1 = 'build' ]; then
     # 执行打包
     pnpm build
     # 复制文件
-    cp -r -f ./dist/ $outPath
+    cp -r -f ./.vitepress/dist/ $outPath
     echo '打包完成';
 
     cd $outPath
     git add .
     git commit -am "deploy by June at `date '+%Y-%m-%d %H:%M:%S'`"
-    git push -f origin master
+    git push -f origin main
     echo '提交完成';
 else
     exit 1
