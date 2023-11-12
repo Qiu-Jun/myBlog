6c747a43c2fc50232222049e4372a05b0bcae087
June <1601745371@qq.com>
2023-11-12 20:35:34 +0800

    deploy by June at 2023-11-12 20:35:34

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,12 @@
+/*
+ * @Author: June
+ * @Description: 
+ * @Date: 2023-11-06 01:48:03
+ * @LastEditors: June
+ * @LastEditTime: 2023-11-12 20:29:21
+ */
 import BlogTheme from '@sugarat/theme'
 // 自定义样式重载
-// import './style.scss'
+import './style.scss'
 
 export default BlogTheme
